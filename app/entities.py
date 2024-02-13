f6cd3b0b3d502ee7d59085e3dea20e5ea04d959b
Aprendiz <Aprendiz@BOGDFPCGMF4142.FormacionSena.RED>
2024-02-13 17:42:35 -0500

    evidencia 3

--- app/entities.py
+++ app/entities.py
@@ -19,3 +19,23 @@ class Cliente():
         for i in self.lista_carros:
             print("Carro con placas: " + i.placa)
     
+class Cupo():
+    
+    def __init__(self, letra):
+        self.letra = letra
+        
+class Pago():
+    def __init__(self, fecha_inicio, hora_inicio, fecha_fin, hora_fin, valor, carro, cupo, empleado):
+        self.fecha_inicio = fecha_inicio
+        self.hora_inicio = hora_inicio
+        self.fecha_fin = fecha_fin
+        self.hora_fin = hora_fin
+        self.valor = valor
+        self.carro = carro
+        self.cupo = cupo
+        self.empleado = empleado
+        
+class Empleado():
+    def __init__(self, nombre, codigo):
+        self.nombre = nombre
+        self.codigo = codigo  
